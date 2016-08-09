8024b9c2ec8196fe89bbbba5c0113bc738c440b4
Ben Longbons <b.r.longbons@gmail.com>
2016-08-09 13:09:21 -0700

    Create project skeleton

---
+++ include/tqdm/tqdm.h
@@ -0,0 +1,7 @@
+#pragma once
+
+
+extern int check_you_are_linking_properly;
+
+__attribute__((used))
+static int *check_you_are_linking_properly_c = &check_you_are_linking_properly;
---
+++ test/test-example-c.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "tqdm/tqdm.h"
+
+
+int main()
+{
+    printf("Dummy test (C)\n");
+    return 0;
+}

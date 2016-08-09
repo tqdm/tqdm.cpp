88f6a0564bb2fad462b7ed1814df451f8a2e2385
Casper da Costa-Luis <casper.dcl@physics.org>
2016-08-09 23:17:14 +0100

    lint, build fixes

--- include/tqdm/utils.h
+++ include/tqdm/utils.h
@@ -4,10 +4,10 @@
 
 #if !defined(IS_WIN) && (defined(_WIN32) || defined(_WIN64))
 #define IS_WIN
-#elif !defined IS_NIX && (\
-  defined(unix) || defined(__unix) || defined(__unix__) || \
-  defined(__linux__) || defined(__FreeBSD__) || \
-  defined(__APPLE__) || defined(__MACH__))
+#elif !defined IS_NIX &&                                                 \
+    (defined(unix) || defined(__unix) || defined(__unix__) ||            \
+     defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__) || \
+     defined(__MACH__))
 #define IS_NIX
 #endif
 
@@ -16,17 +16,16 @@
 #include <set>
 #include <limits>
 
-namespace tqdm{
+namespace tqdm {
 
 // namespace utils{
 
 template <typename IntType>
 /** TODO */
-_range(IntType a, IntType b=std::numeric_limits<IntType>::max(), IntType step=IntType(1));
-
-// }  // utils
-
-}  // tqdm
+class _range {
+  static const IntType IntTypeMax = std::numeric_limits<IntType>::max();
+  _range(IntType a, IntType b = IntTypeMax, IntType step = IntType(1));
+};
 
 // colorama win
 // weakset
@@ -35,12 +34,16 @@ _range(IntType a, IntType b=std::numeric_limits<IntType>::max(), IntType step=In
 // _supports_unicode(file)
 // _environ_cols()
 // _sh(const char *cmd[], ...)
-const char *_term_move_up()
-{
+const char *_term_move_up() {
   return
 #if defined(IS_WIN) && !defined(colorama)
-  ""
+      ""
 #else
-  "\x1b[A";
+      "\x1b[A"
 #endif
+      ;
 }
+
+// }  // utils
+
+}  // tqdm
